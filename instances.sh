5efab293b9999dc42fa0bb616cce9e0a637b418c
ajay <vallala1602@gmail.com>
2025-05-22 12:00:37 +0530

    aws-instances

--- instances.sh
+++ instances.sh
@@ -18,7 +18,7 @@ do
     --query "Instances[0].InstanceId" \
     --output text)
 
-    if [ $instance != "frontend" ]
+    if [ "${instance,,}" != "frontend" ]
     then 
      IP=$(aws ec2 describe-instances \
        --instance-ids $INSTANCE_ID \
