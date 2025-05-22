a5a1bbce9931811e004d5e4c3388dad30b777054
ajay <vallala1602@gmail.com>
2025-05-22 16:26:11 +0530

    aws-instances

--- roboshop.sh
+++ roboshop.sh
@@ -39,7 +39,7 @@ do
     "Changes": [{
       "Action": "UPSERT",
       "ResourceRecordSet": {
-        "Name": "$instance.$DOMAIN_NAME",
+        "Name": $instance.$DOMAIN_NAME,
         "Type": "A",
         "TTL": 1,
         "ResourceRecords": [{"Value": $IP}]
