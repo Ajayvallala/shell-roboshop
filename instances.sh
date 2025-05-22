3114689bac83b865e99d359051a586d286054ad2
ajay <vallala1602@gmail.com>
2025-05-22 11:33:55 +0530

    aws-instances

--- instances.sh
+++ instances.sh
@@ -3,16 +3,31 @@
 AMIID="ami-09c813fb71547fc4f"
 SG_ID="sg-007aa1e4ce81005d7"
 INSTANCE_TYPE="t2.micro"
-INSTANCES=("mongodb" "catalogur" "frontend")
-
-
+INSTANCES=("mongodb" "catalogue")
+SUBNET_ID="subnet-07d9ef0ea659b9697"
 
 for instance in ${INSTANCES[@]}
 do 
-    aws ec2 run-instances \
+    INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMIID \
     --instance-type $INSTANCE_TYPE \
     --security-group-ids $SG_GROUP \
-    --subnet-id subnet-07d9ef0ea659b9697 \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
+    --subnet-id $SUBNET_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query "Reservations[0].Instances[0].InstanceId" \
+    --output text)
+
+    if [ $instance != frontend]
+    then 
+     IP=$(aws ec2 describe-instances \
+       --instance-ids $INSTANCE_ID \
+       --query "Reservations[0].Instances[0].PrivateIpAddress" \
+       --output text)
+    else
+     IP=$(aws ec2 describe-instances \
+       --instance-ids $INSTANCE_ID \
+       --query "Reservations[0].Instances[0].PublicIpAddress" \
+       --output text)
+    fi
+    echo "$instance = $IP"
 done
