4f7d940838a2b86e1b6f882c34aa5580bbd4e90d
ajay <vallala1602@gmail.com>
2025-05-22 11:08:49 +0530

    aws-instances

--- instances.sh
+++ instances.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 AMIID="ami-09c813fb71547fc4f"
-SG_GROUP="sg-007aa1e4ce81005d7"
+SG_ID="sg-007aa1e4ce81005d7"
 INSTANCE_TYPE="t2.micro"
 INSTANCES=("mongodb" "catalogur" "frontend")
 
 
-for instances in ${INSTANCES[@]}
+
+for instance in ${INSTANCES[@]}
 do 
     aws ec2 run-instances \
     --image-id $AMIID \
     --instance-type $INSTANCE_TYPE \
     --security-group-ids $SG_GROUP \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instances}]"
+    --subnet-id subnet-07d9ef0ea659b9697 \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"
 done
