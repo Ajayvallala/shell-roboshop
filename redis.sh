9a6088a7838940ee7fe1a2d1467573904942ffb4
ajay <vallala1602@gmail.com>
2025-05-23 15:37:56 +0530

    redis.sh

--- redis.sh
+++ redis.sh
@@ -48,7 +48,7 @@ VALIDATE $? "Installing redis"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 VALIDATE $? "Opeaning all the traffic"
 
-sed -i 's/protected-mode yes/protected-mode no' /etc/redis/redis.conf
+sed -i 's/protected-mode yes/protected-mode no/' /etc/redis/redis.conf
 VALIDATE $? "Disabling protected-mode"
 
 systemctl enable redis &>>$LOG_FILE
